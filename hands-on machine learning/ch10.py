2aaeca00d5434a2e217b5e4f0f72e124718485d0
LJY <leeshim66@naver.com>
2022-01-07 18:24:07 +0900

    손실함수, 정확도 시각화 회귀 문제에 딥러닝 적용

--- hands-on machine learning/ch10.py
+++ hands-on machine learning/ch10.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tensorflow import keras
+import tensorflow as tf
 
 x_train = np.array([1., 2., 3., 4., 5., 6.])
 y_train = np.array([9., 16., 23., 30., 37., 44.])
@@ -31,19 +31,54 @@ print('b: {:10f}'.format(b))
 print('result : ', x_train * W + b)
 
 
-
-fashion_mnist = keras.datasets.fashion_mnist
+# 데이터 로드
+fashion_mnist = tf.keras.datasets.fashion_mnist
 (X_train_full, y_train_full), (X_test,y_test) = fashion_mnist.load_data()
 
-X_valid,X_train = X_train_full[:5000]/255.0, X_train_full[5000:]/255.0
-y_valid,y_train = y_train_full[:5000], y_train_full[5000:]
-X_test = X_test/255.0
+X_valid,X_train = X_train_full[:10000]/255, X_train_full[10000:]/255
+y_valid,y_train = y_train_full[:10000], y_train_full[10000:]
+X_test = X_test/255
 
 class_names = ['T-shirt/top', 'Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
 
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=[28,28]))
-model.add(keras.layers.Dense(300, activation='relu'))
-model.add(keras.layers.Dense(100, activation='relu'))
-model.add(keras.layers.Dense(10, activation='softmax'))
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=[28,28]))
+model.add(tf.keras.layers.Dense(300, activation='relu'))
+model.add(tf.keras.layers.Dense(100, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid,y_valid))
+model.evaluate(X_test,y_test)
+
+# 손실함수, 정확도 시각화
+pd.DataFrame(history.history).plot(figsize=(8,5))
+plt.grid(True)
+plt.gca().set_ylim(0,1)
+plt.show()
+
+
+# 회귀 문제에 딥러닝 적용
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.fit_transform(X_valid)
+X_test = scaler.fit_transform(X_test)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(30, activation='relu', input_shape=X_train.shape[1:]))
+model.add(tf.keras.layers.Dense(1))
+model.compile(loss='mean_squared_error',optimizer='sgd')
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid,y_valid))
+model.evaluate(X_test, y_test)
+
+y_pred = model.predict(X_test)
+
