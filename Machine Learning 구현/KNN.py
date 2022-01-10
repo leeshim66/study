1a53f2aa92d5be1c131dbf8e4aaa88ac4b040ec7
LJY <leeshim66@naver.com>
2022-01-10 23:26:37 +0900

    KNN 구현

--- Machine Learning 구현/KNN.py
+++ Machine Learning 구현/KNN.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-
 class knn_alg() :
     def __init__(self, n_neighbors):
         self.k = n_neighbors
@@ -9,13 +6,13 @@ class knn_alg() :
         euclid = np.sqrt(np.sum((x1-x2)**2))
         return euclid
 
-    def euclidean_matrix(self,x_test) :
+    def euclidean_matrix(self,test_x) :
         n_train = len(self.x)
-        n_test = len(x_test)
+        n_test = len(test_x)
         self.mat = pd.DataFrame(index=range(0,n_train), columns=range(0,n_test))
         for i in range(n_train):
             for j in range(n_test):
-                self.mat[j][i] = self.euclidean(x_test.iloc[j,:],self.x.iloc[i,:])
+                self.mat[j][i] = self.euclidean(test_x.iloc[j,:],self.x.iloc[i,:])
         return self.mat
 
     def fit(self,x,y):
@@ -25,6 +22,8 @@ class knn_alg() :
     def predict(self,x_test):
         y_hat = []
         euc = self.euclidean_matrix(x_test)
-        for i in range(len(euc)):
-
+        rank_mat = euc.rank(ascending=False).astype(int)
+        for i in rank_mat.columns:
+            idx = rank_mat[i].sort_values().index[:self.k]
+            y_hat.append(np.mean(self.y[idx]))
         return y_hat
