14a29c67b4b72375595f859494617b1ad0548e45
LJY <leeshim66@naver.com>
2022-01-13 12:35:47 +0900

    KNN 구현

--- Machine Learning 구현/KNN.py
+++ Machine Learning 구현/KNN.py
@@ -5,32 +5,25 @@ class knn_alg() :
     def __init__(self, n_neighbors):
         self.k = n_neighbors
 
-    def euclidean(self,x1,x2) :
-        euclid = np.sqrt(np.sum((x1-x2)**2))
-        return euclid
-
-    def euclidean_matrix(self,test_x) :
+    def euclidean_matrix(self,x_test) : # x_train과 x_test 사이의 유클리드 거리 행렬 생성
         n_train = len(self.x)
-        n_test = len(test_x)
+        n_test = len(x_test)
         self.mat = pd.DataFrame(index=range(0,n_train), columns=range(0,n_test))
         for i in range(n_train):
             for j in range(n_test):
-                self.mat[j][i] = self.euclidean(test_x.iloc[j,:],self.x.iloc[i,:])
-        self.mat.index = self.x.index
+                self.mat[j][i] = np.sqrt(sum((x_test.iloc[j]-self.x.iloc[i])**2))
+        self.mat.index = self.x.index # x_train과 y_train 인덱스 통일
         return self.mat
 
-    def fit(self,x,y):
-        self.x = x
-        self.y = y
+    def fit(self,x_train,y_train): # KNN은 비모수 학습
+        self.x = x_train
+        self.y = y_train
 
     def predict(self,x_test):
         y_hat = []
-        euc = self.euclidean_matrix(x_test)
-        rank_mat = euc.rank(ascending=False).astype(int)
+        euc = self.euclidean_matrix(x_test.reset_index(drop=True))
+        rank_mat = euc.rank().astype(int) # 열별 x_train과의 거리 오름차순
         for i in rank_mat.columns:
-            idx = rank_mat[i].sort_values().index[:self.k]
-            if np.mean(self.y[idx])>0.5 :
-                y_hat.append(1)
-            else :
-                y_hat.append(0)
+            idx = rank_mat[i].sort_values().index[:self.k] # 열별 정렬 후 최솟값(가장 가까운) 인덱스 추출
+            y_hat.append(round(np.mean(self.y[idx]),10))
         return y_hat
