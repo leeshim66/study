b35fd519212ea1dd82fa8a90f2bfb91a21ac3913
LJY <leeshim66@naver.com>
2024-02-18 23:13:58 +0900

    알고리즘 연습

---
+++ programmers/숫자 변환하기.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(x, y, n):
+    dist = [0] * 1000001
+    q = deque()
+    dist[x] = 1
+    q.append(x)
+
+    while q:
+        x = q.popleft()
+        if 0 <= x + n <= 1000000 and dist[x + n] == 0:
+            dist[x + n] = dist[x] + 1
+            q.append(x + n)
+        if 0 <= x * 2 <= 1000000 and dist[x * 2] == 0:
+            dist[x * 2] = dist[x] + 1
+            q.append(x * 2)
+        if 0 <= x * 3 <= 1000000 and dist[x * 3] == 0:
+            dist[x * 3] = dist[x] + 1
+            q.append(x * 3)
+
+    return dist[y]-1
+
+x = 10
+y = 40
+n = 5
+answer = 2
