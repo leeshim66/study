646608b163a6fa70d48aab39bb25b5c2e4878b75
LJY <leeshim66@naver.com>
2024-02-20 23:06:49 +0900

    알고리즘 연습

---
+++ programmers/택배상자.py
@@ -0,0 +1,15 @@
+def solution(order):
+    stack = []
+    length = len(order)
+    index = 0
+    for num in range(1, length + 1):
+        stack.append(num)
+
+        while stack and stack[-1] == order[index]:
+            stack.pop()
+            index += 1
+
+    return index
+
+order = [4,3,1,2,5]
+answer = 2
